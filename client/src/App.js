0e4a1cff7a405ba61f52a6ec5b230fdf4166dcb9
shajjad Shagor <shajjadshagor@gmail.com>
2023-01-13 09:34:16 -0800

    set customers route in server and set client page with data grid and design table.. data fetch from server and shown in table

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Dashboard from "scenes/dashboard";
 import Layout from "scenes/layout";
 import { themeSettings } from "theme";
 import Products from "scenes/products/index";
+import Customers from "scenes/customers/index";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -22,6 +23,7 @@ function App() {
               <Route path="/" element={<Navigate to='/dashboard' replace />} />
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/products" element={<Products />} />
+              <Route path="/customers" element={<Customers />} />
             </Route>
           </Routes>
         </ThemeProvider>
