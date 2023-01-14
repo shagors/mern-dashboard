cd2fd629da9da5a759609a175f836e3a7f957bcc
shajjad Shagor <shajjadshagor@gmail.com>
2023-01-13 20:51:01 -0800

    design server side for transaction and data send to mongodb

--- server/index.js
+++ server/index.js
@@ -14,7 +14,8 @@ import salesRoutes from './routes/sales.js';
 import User from './models/User.js';
 import Product from './models/Product.js';
 import ProductStat from './models/ProductStat.js';
-import { dataUser, dataProduct, dataProductStat } from "./data/index.js";
+import Transaction from './models/Transaction.js';
+import { dataUser, dataProduct, dataProductStat, dataTransaction } from "./data/index.js";
 
 
 /* Configuration */ 
@@ -34,6 +35,7 @@ app.use("/general", generalRoutes);
 app.use("/management", managementRoutes);
 app.use("/sales", salesRoutes);
 
+
 /* MONGOOSE */
 const PORT = process.env.PORT || 9000;
 mongoose.connect(process.env.MONGO_URL, {
@@ -45,5 +47,6 @@ mongoose.connect(process.env.MONGO_URL, {
     /* Only add data one time from json file */ 
     // Product.insertMany(dataProduct);
     // ProductStat.insertMany(dataProductStat);
+    // Transaction.insertMany(dataTransaction);
     // User.insertMany(dataUser);
 }).catch((error) => console.log(`${error} did not connect`));
